e2e059fb0daafd0e8fd95f034e3b150de6f21bf5
Louis Muhammad <louis.muhammad@gmail.com>
2016-11-27 12:50:30 -0500

    Added Angular 2 with TypeScript

--- src/api/app.rb
+++ src/api/app.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/config_file'
 require 'sinatra/content_for'
+require './config/app-config'
+
+ENV['RACK_ENV'] ||= 'development'
+set :public_folder, Config['public_dir']
+enable :sessions
+set :session_secret, Config['session_secret']
 
 configure do
 	if ENV['DATABASE_URL'].blank?
@@ -20,6 +26,6 @@ configure do
 	$stdout.sync = true
 
 	get '/'do
-		"Welcome to Study Abroad Pathways"
+		send_file File.join(settings.public_folder, 'index.html')
 	end
 end
---
+++ src/ui/app/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'sap-app'
+})
+
+@View({
+  template: '<h2>Welcome to Study Abroad Pathways</h2>'
+})
+
+export class AppComponent {
+
+}
